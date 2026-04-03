04655ceae95df363526fd8ebb2ed4ba453e847d1
Rank-8_RK <djcraes28@gmail.com>
2026-04-03 07:45:26 +0200

    Create state.js

---
+++ js/core/state.js
@@ -0,0 +1,19 @@
+// ===== GLOBAL STATE =====
+let players = [];
+let currentPlayer = 0;
+let turn = 1;
+let actionTracker = {};
+
+let gameMode = "turns";
+let modeValue = 20;
+
+let playerColors = ["#ff4c4c","#4caf50","#2196f3","#ff9800"];
+
+let stocks = [
+  { name: "CDJ", price: 10, volatility: 0.20, owned: {}, totalSpent: {}, desc: "Clothing company", history:[10] },
+  { name: "Panda & Co.", price: 10, volatility: 0.12, owned: {}, totalSpent: {}, desc: "Stable bank", history:[10] },
+  { name: "GRAY-BOX", price: 10, volatility: 0.10, owned: {}, totalSpent: {}, desc: "Insurance", history:[10] },
+  { name: "BA", price: 10, volatility: 0.40, owned: {}, totalSpent: {}, desc: "Volatile sports brand", history:[10] },
+  { name: "SEED", price: 10, volatility: 0.25, owned: {}, totalSpent: {}, desc: "Agriculture", history:[10] },
+  { name: "EXTRA FRESH", price: 10, volatility: 0.20, owned: {}, totalSpent: {}, desc: "Food company", history:[10] }
+];

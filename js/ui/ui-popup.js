ba4fc63f6f39b0c47ed2b936474e7c22c8e626fc
Rank-8_RK <djcraes28@gmail.com>
2026-04-03 07:47:31 +0200

    Create ui-popup.js

---
+++ js/ui/ui-popup.js
@@ -0,0 +1,11 @@
+function popup(html) {
+  const el = document.getElementById("popup");
+  const content = document.getElementById("popupContent");
+
+  content.innerHTML = html + `<br><button onclick="closePopup()">OK</button>`;
+  el.classList.remove("hidden");
+}
+
+function closePopup(){
+  document.getElementById("popup").classList.add("hidden");
+}

02f97a11f10a441059d0af94b2c0d9e057abb506
Rank-8_RK <djcraes28@gmail.com>
2026-04-04 12:45:07 +0200

    Update ui-trade.js

--- js/ui/ui-trade.js
+++ js/ui/ui-trade.js
@@ -11,20 +11,20 @@ function trade(i, amount){
 }
 
 function buy(i, amount){
-  let s = stocks[i];
+  let s = GS.stocks[i];
   let cost = s.price * amount;
-  if(players[currentPlayer].money < cost) return popup("Not enough money");
+  if(GS.players[GS.currentPlayer].money < cost) return popup("Not enough money");
 
-  players[currentPlayer].money -= cost;
-  s.owned[currentPlayer] += amount;
+  GS.players[GS.currentPlayer].money -= cost;
+  s.owned[GS.currentPlayer] += amount;
   render();
 }
 
 function sell(i, amount){
-  let s = stocks[i];
-  if(s.owned[currentPlayer] < amount) return popup("Not enough shares");
+  let s = GS.stocks[i];
+  if(s.owned[GS.currentPlayer] < amount) return popup("Not enough shares");
 
-  s.owned[currentPlayer] -= amount;
-  players[currentPlayer].money += s.price * amount;
+  s.owned[GS.currentPlayer] -= amount;
+  GS.players[GS.currentPlayer].money += s.price * amount;
   render();
 }
